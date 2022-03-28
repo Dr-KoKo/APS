42148e364b525aa66ba54cfed2720861c79924cb
Denny-Ko <97681286+Denny-Ko@users.noreply.github.com>
2022-03-28 22:57:37 +0900

    Updated on Mar 28 2022 22:58

--- src/baekjoon/b2805/B2805.java
+++ src/baekjoon/b2805/B2805.java
@@ -7,11 +7,12 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B2805 {
+	private static int N;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer input = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(input.nextToken());
+		N = Integer.parseInt(input.nextToken());
 		int M = Integer.parseInt(input.nextToken());
 
 		int[] heights = new int[N];
@@ -20,7 +21,8 @@ public class B2805 {
 		input = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			heights[i] = Integer.parseInt(input.nextToken());
-			max = Math.max(max, heights[i]);
+			if(max<heights[i])
+				max = heights[i];
 		}
 
 		int ans = find(heights, 0, max, M);
@@ -32,19 +34,20 @@ public class B2805 {
 	private static int find(int[] heights, int start, int end, int M) {
 		while (start < end) {
 			int mid = (start + end) / 2;
-			if (cut(heights, mid) > M)
+			long sum = cut(heights, mid);
+			if (sum > M)
 				start = mid;
-			else if (cut(heights, mid) < M)
+			else if (sum < M)
 				end = mid - 1;
-			else if (cut(heights, mid) == M)
+			else
 				return mid;
 		}
 		return start;
 	}
 
-	private static int cut(int[] heights, int h) {
-		int ret = 0;
-		for (int i = 0; i < heights.length; i++) {
+	private static long cut(int[] heights, int h) {
+		long ret = 0;
+		for (int i = 0; i < N; i++) {
 			if (heights[i] - h > 0)
 				ret += heights[i] - h;
 		}
