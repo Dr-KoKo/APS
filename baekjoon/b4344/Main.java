58a6924d50d9f8f5fdb5d02953fde23401e7d6c6
Denny-Ko <97681286+Denny-Ko@users.noreply.github.com>
2022-05-17 01:18:01 +0900

    updated on May 17 01:17

--- baekjoon/b4344/Main.java
+++ baekjoon/b4344/Main.java
@@ -1,5 +1,38 @@
 package b4344;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int C = input.nextInt();
+
+		for (int c = 1; c <= C; c++) {
+
+			int N = input.nextInt();
+
+			int[] scores = new int[N];
+			for (int i = 0; i < N; i++) {
+				scores[i] = input.nextInt();
+			}
+
+			double avg = 0;
+			for (int i = 0; i < N; i++) {
+				avg += scores[i];
+			}
+			avg = avg / (double) N;
+
+			int cnt = 0;
+			for (int i = 0; i < N; i++) {
+				if (scores[i] > avg) {
+					cnt++;
+				}
+			}
+
+			double ans = (double) cnt / N;
+			System.out.printf("%.3f%%\n", Math.round(ans * 1000*100) / 1000.0);
+
+		}
+	}
 }
