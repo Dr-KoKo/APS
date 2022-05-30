348849f8e0f076f05211f691ad8265cf9b806a57
Denny-Ko <97681286+Denny-Ko@users.noreply.github.com>
2022-05-31 00:04:35 +0900

    아스키 코드

---
+++ baekjoon/b11654/Main_1.java
@@ -0,0 +1,14 @@
+package b11654;
+
+import java.util.Scanner;
+
+public class Main_1 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int ch = input.next().charAt(0) - '\u0000';
+		
+		System.out.println(ch);
+		
+	}
+}
