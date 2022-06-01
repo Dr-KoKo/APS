91d606d81868aa9441cd26f30592e6f10930a7f2
Denny-Ko <97681286+Denny-Ko@users.noreply.github.com>
2022-06-01 23:59:36 +0900

    셀프 넘버

--- baekjoon/b4673/Main.java
+++ baekjoon/b4673/Main.java
@@ -1,52 +1,46 @@
 package b4673;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
 public class Main {
+
+	static boolean[] check = new boolean[10001];
+
 	public static void main(String[] args) {
-		Integer[] arr1 = new Integer[10001];
-		for (int i = 0; i < arr1.length; i++) {
-			arr1[i] = i;
-		}
-		List<Integer> arr2 = new LinkedList<>(Arrays.asList(arr1));
-		arr2.remove(0);
 
-		for (int i = 1; i < arr1.length; i++) {
-			Integer temp = i;
+		for (int i = 1; i <= 10000; i++) {
+
+			int self = i;
+
 			while (true) {
-				temp = temp + (temp / 1000) + ((temp % 1000) / 100) + ((temp % 100) / 10) + (temp % 10);
-				arr2.remove(temp);
-				System.out.println(temp);
-				if (temp.intValue() > arr1.length)
+				self = self(self);
+
+				if (self == i || self > 10000) {
 					break;
+				}
+
+				check[self] = true;
+			}
+
+		}
+
+		for (int i = 1; i <= 10000; i++) {
+			if (!check[i]) {
+				System.out.println(i);
 			}
 		}
 
-		for (int i = 0; i < arr2.size(); i++) {
-			System.out.println(arr2.get(i).toString());
-		}
-
-		int[] arr = new int[10001];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = i;
-		}
-
-		for (int i = 1; i < arr.length; i++) {
-			int temp = i;
-			while (true) {
-				temp = temp + (temp / 1000) + (temp / 100) + (temp / 10) + (temp % 10);
-				if (temp < arr.length)
-					arr[temp] = 0;
-				else
-					break;
-			}
-		}
-
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] != 0)
-				System.out.println(arr[i]);
-		}
 	}
+
+	private static int self(int N) {
+
+		int ret = N;
+
+		while (N / 10 != 0 || N % 10 != 0) {
+			ret += (N % 10);
+			N /= 10;
+		}
+
+		return ret;
+
+	}
+
 }
