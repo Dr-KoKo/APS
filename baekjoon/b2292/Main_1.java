ab18d61712382b8a4a3b93b14a7782bad1f2ae7c
Denny-Ko <97681286+Denny-Ko@users.noreply.github.com>
2022-06-05 22:11:14 +0900

    벌집

--- baekjoon/b2292/Main_1.java
+++ baekjoon/b2292/Main_1.java
@@ -2,21 +2,19 @@ package b2292;
 
 import java.util.Scanner;
 
-public class B2292 {
+public class Main_1 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		int N = input.nextInt();
-		
+
 		int cnt = 1;
 		int max = 1;
-		
-		while(N>max) {
-			max += 6*(cnt++);
+
+		while (N > max) {
+			max += 6 * (cnt++);
 		}
-		
+
 		System.out.println(cnt);
-		
-		input.close();
 	}
 }
