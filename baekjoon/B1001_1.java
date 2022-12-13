6b372dd41dd948170a4689c71b4bb0998db1500e
Denny-Ko <donghar@naver.com>
2022-12-13 18:00:24 +0900

    BOJ: 1001(A-B)
    updated on Dec 13 2022

--- baekjoon/B1001_1.java
+++ baekjoon/B1001_1.java
@@ -1,8 +1,8 @@
-package b1001;
+package baekjoon;
 
 import java.util.Scanner;
 
-public class B1001 {
+public class B1001_1 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int A = input.nextInt();
