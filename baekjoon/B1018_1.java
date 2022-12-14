f66c66c8c2edc7192efc43f1059e3eff85ca184b
Denny-Ko <donghar@naver.com>
2022-12-14 21:46:47 +0900

    BOJ: 1018(체스판 다시 칠하기)
    updated on Dec 14 2022

--- baekjoon/B1018_1.java
+++ baekjoon/B1018_1.java
@@ -1,14 +1,26 @@
-package b1018;
+package baekjoon;
 
 import java.util.Scanner;
 
-public class B1018 {
-
+public class B1018_1 {
 	private static int N, M;
 	private static int[][] board;
 
 	public static void main(String[] args) {
+		input();
 
+		int ans = Integer.MAX_VALUE;
+
+		for (int i = 0; i <= N - 8; i++) {
+			for (int j = 0; j <= M - 8; j++) {
+				ans = Math.min(ans, countWork(board, i, j));
+			}
+		}
+
+		System.out.println(ans);
+	}
+
+	private static void input() {
 		Scanner input = new Scanner(System.in);
 
 		N = input.nextInt();
@@ -18,51 +30,29 @@ public class B1018 {
 		board = new int[N][M];
 
 		for (int i = 0; i < N; i++) {
-
 			char[] str = input.nextLine().toCharArray();
 
 			for (int j = 0; j < M; j++) {
-
 				if (str[j] == 'W')
 					board[i][j] = 0;
 				else if (str[j] == 'B')
 					board[i][j] = 1;
 				else
 					System.out.println("error");
-
 			}
-
 		}
-
-		int ans = Integer.MAX_VALUE;
-
-		for (int i = 0; i <= N - 8; i++) {
-
-			for (int j = 0; j <= M - 8; j++) {
-
-				ans = Math.min(ans, countWork(board, i, j));
-
-			}
-
-		}
-
-		System.out.println(ans);
 
 		input.close();
-
 	}
 
 	private static int countWork(int[][] target, int x, int y) {
-
 		int curr = 1;
 
 		int retW = 0;
 		int retB = 0;
 
 		for (int i = 0; i < 8; i++) {
-
 			for (int j = 0; j < 8; j++) {
-
 				curr = 1 - curr;
 
 				int nc = x + i;
@@ -74,15 +64,12 @@ public class B1018 {
 					++retW;
 				else
 					System.out.println("error2");
-
 			}
 
 			curr = 1 - curr;
-
 		}
 
 		return Math.min(retW, retB);
-
 	}
 
 }
