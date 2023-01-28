1998b5642907eb3d7b361e30b5e840611e7e045a
Denny-Ko <donghar@naver.com>
2023-01-29 02:36:53 +0900

    BOJ: 9663(N-Queen)
    updated on Jan 29th 2023

--- baekjoon/B9663_2.java
+++ baekjoon/B9663_2.java
@@ -38,17 +38,6 @@ public class B9663_2 {
         }
     }
 
-    private static boolean valid() {
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j < i; j++) {
-                if (attackable(i, col[i], j, col[j])) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
     private static boolean attackable(int r1, int c1, int r2, int c2) {
         if (c1 == c2) return true;
         if (r1 - c1 == r2 - c2) return true;
